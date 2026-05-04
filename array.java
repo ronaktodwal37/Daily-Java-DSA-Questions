1d726bde25c47bd68529f2e174261801b0d7f1d8
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-05-04 22:38:10 +0530

    Day 1: Solved Two Sum

--- array.java
+++ array.java
@@ -1,5 +1,88 @@
-public class array {
-    public static void main(String[] args) {
-       System.out.println("hello world");
+// 1. Two Sum
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr = new int [2];
+        for(int i=0;i<nums.length;i++){
+            for (int j=0;j<nums.length;j++){
+                if(i!=j){
+                    if(nums[i]+nums[j] == target){
+                        arr[0]=i;
+                        arr[1]=j;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+}
+
+
+// 75. Sort Colors
+
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+// You must solve this problem without using the library's sort function.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+// Example 2:
+
+// Input: nums = [2,0,1]
+// Output: [0,1,2]
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 300
+// nums[i] is either 0, 1, or 2.
+
+class Solution {
+    public void sortColors(int[] nums) {
+       for(int i=0;i<nums.length-1;i++){
+        for(int j=0;j<nums.length-i-1;j++){
+            if(nums[j]>nums[j+1]){
+                int temp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = temp;
+            }
+        }
+       }
+
+
     }
 }
