0d946a260ff1ac71132caf80a38ccbebf8c30628
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-05-05 23:29:48 +0530

    solve the question of daily

--- array.java
+++ array.java
@@ -71,18 +71,137 @@ class Solution {
 // 1 <= n <= 300
 // nums[i] is either 0, 1, or 2.
 
+// class Solution {
+//     public void sortColors(int[] nums) {
+//        for(int i=0;i<nums.length-1;i++){
+//         for(int j=0;j<nums.length-i-1;j++){
+//             if(nums[j]>nums[j+1]){
+//                 int temp = nums[j];
+//                 nums[j] = nums[j+1];
+//                 nums[j+1] = temp;
+//             }
+//         }
+//        }
+
+
+//     }
+// }
+
+
+
+// 287. Find the Duplicate Number
+
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+// There is only one repeated number in nums, return this repeated number.
+
+// You must solve the problem without modifying the array nums and using only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+// Example 2:
+
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+// Example 3:
+
+// Input: nums = [3,3,3,3,3]
+// Output: 3
+ 
+
+// Constraints:
+
+// 1 <= n <= 105
+// nums.length == n + 1
+// 1 <= nums[i] <= n
+// All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ 
+
+// import java.util.*;
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+
+
+// int count =0;
+// int ans=0;
+// HashMap<Integer,Integer> map = new HashMap<>();
+// for(int i=0;i<nums.length;i++){
+//     if(map.containsKey(nums[i])){
+//          map.put(nums[i],map.get(nums[i])+1); 
+//     }
+//     else{
+//           map.put(nums[i],1);
+//     }
+// }
+
+//  for(Map.Entry<Integer,Integer> e: map.entrySet()) {
+//     if(e.getValue() > 1){
+//         ans = e.getKey();
+//     }
+//         }
+//         return ans;
+//     }
+// }
+
+
+// 121. Best Time to Buy and Sell Stock
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+// Example 2:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+// Constraints:
+
+// 1 <= prices.length <= 105
+// 0 <= prices[i] <= 104
+
+
+
 class Solution {
-    public void sortColors(int[] nums) {
-       for(int i=0;i<nums.length-1;i++){
-        for(int j=0;j<nums.length-i-1;j++){
-            if(nums[j]>nums[j+1]){
-                int temp = nums[j];
-                nums[j] = nums[j+1];
-                nums[j+1] = temp;
-            }
+    public int maxProfit(int[] prices) {
+       int maxprofit = 0 ;
+       int min = Integer.MAX_VALUE;
+       for(int i=0;i<prices.length;i++){
+        if(prices[i]<min){
+            min = prices[i];
+        }
+        else{
+            int profit = prices[i] - min;
+             maxprofit = Math.max(maxprofit, profit);
         }
        }
 
 
+    //    2nd approach (but time limit exceed)
+    // for(int i=0;i<prices.length;i++){
+    //     for(int j=i+1;j<prices.length;j++){
+    //         int currentprofit = prices[j]-prices[i];
+    //         if(currentprofit>maxprofit){
+    //             maxprofit = currentprofit ;
+    //         }
+    //     }
+    // }
+       return maxprofit;
     }
 }
