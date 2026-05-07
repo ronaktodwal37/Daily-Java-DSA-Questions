9ba871a09046684bca6a896082df520c16a7eaa8
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-05-07 23:23:02 +0530

    Day 4:leetcode

--- array.java
+++ array.java
@@ -27,22 +27,22 @@
 // -109 <= target <= 109
 // Only one valid answer exists.
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] arr = new int [2];
-        for(int i=0;i<nums.length;i++){
-            for (int j=0;j<nums.length;j++){
-                if(i!=j){
-                    if(nums[i]+nums[j] == target){
-                        arr[0]=i;
-                        arr[1]=j;
-                    }
-                }
-            }
-        }
-        return arr;
-    }
-}
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int[] arr = new int [2];
+//         for(int i=0;i<nums.length;i++){
+//             for (int j=0;j<nums.length;j++){
+//                 if(i!=j){
+//                     if(nums[i]+nums[j] == target){
+//                         arr[0]=i;
+//                         arr[1]=j;
+//                     }
+//                 }
+//             }
+//         }
+//         return arr;
+//     }
+// }
 
 
 // 75. Sort Colors
@@ -230,8 +230,8 @@ class Solution {
 // 1 <= nums.length <= 104
 // -231 <= nums[i] <= 231 - 1
 
-class Solution {
-    public void moveZeroes(int[] nums) {
+// class Solution {
+//     public void moveZeroes(int[] nums) {
         // int nonzero = 0;
         // for(int i=0;i<nums.length;i++){
         //     if(nums[i] != 0){
@@ -251,22 +251,111 @@ class Solution {
         //     nums[i] = 0;
         // }
 
-        int nonzero = 0;
-        int count = 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] != 0){
-                nonzero++;
-            }
-        }
+//         int nonzero = 0;
+//         int count = 0;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i] != 0){
+//                 nonzero++;
+//             }
+//         }
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-nums[count] =nums[i];
-count++;
-            }
-        }
-        for(int i=count;i<nums.length;i++){
-            nums[i] = 0;
-        }
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]!=0){
+// nums[count] =nums[i];
+// count++;
+//             }
+//         }
+//         for(int i=count;i<nums.length;i++){
+//             nums[i] = 0;
+//         }
+//     }
+// }
+
+
+
+// 53. Maximum Subarray
+
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+
+// Example 1:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+// Example 2:
+
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+// Example 3:
+
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+ 
+
+// Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+   int maxsum = Integer.MIN_VALUE;
+//    for(int i=0;i<nums.length;i++){
+//    int currentsum = 0;
+//     for(int j=i;j<nums.length;j++){
+//          for(int k = i; k<=j;k++){
+//             currentsum = currentsum + nums[k];
+//          }
+//       if(currentsum>maxsum){
+//         maxsum = currentsum;
+//       }
+//       }
+//      }
+int currentsum = 0;
+for(int i =0;i<nums.length;i++){
+currentsum = currentsum + nums[i];
+
+if(currentsum>maxsum){
+    maxsum = currentsum;
+}
+if(currentsum<0){
+    currentsum = 0;
+}
+}
+     return maxsum;
+
+
+
+
+
+
+
+
+
+
+// int maxsum = Integer.MIN_VALUE;
+// for(int i=0;i<nums.length;i++){
+// int currentsum = nums[i];
+
+// for(int j=i+1;j<nums.length;j++){
+//     currentsum = currentsum + nums[j];
+//     if(currentsum<0){
+//         currentsum = 0;
+//     }
+//     else{
+//         if(currentsum>maxsum){
+//             maxsum = currentsum;
+//         }
+//     }
+// }
+// }
+// return maxsum;
     }
 }
