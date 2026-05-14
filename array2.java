33d0c42369f142ed7978d856eef5a5e115972d96
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-05-14 23:40:22 +0530

    Day X: question name

--- array2.java
+++ array2.java
@@ -300,5 +300,65 @@ public class array2 {
 
 //     }
 // }
+
+
+
+
+// 42. Trapping Rain Water
+
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+
+// Example 1:
+
+// Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+// Output: 6
+// Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+// Example 2:
+
+// Input: height = [4,2,0,3,2,5]
+// Output: 9
+ 
+
+// Constraints:
+
+// n == height.length
+// 1 <= n <= 2 * 104
+// 0 <= height[i] <= 105
+
+
+
+
+// import java.util.*;
+// class Solution {
+//     public int trap(int[] height) {
+
+//         int totalwater = 0;
+//         int[] left = new int[height.length];
+//         int[] right = new int[height.length];
+//         int leftmax = height[0];
+//         int rightmax = height[height.length-1];
+//         for(int i=0;i<height.length;i++){
+//             if(height[i]>leftmax){
+//                 leftmax = height[i];
+//             }
+//             left[i] = leftmax;
+//         }
+//         for(int i=height.length-1;i>=0;i--){
+//             if(height[i]>rightmax){
+//                 rightmax = height[i];
+//             }
+//             right[i] = rightmax;
+//         }
+// for(int i=0;i<height.length;i++){
+//     int length = Math.min(left[i],right[i]);
+//     totalwater = totalwater+ length-height[i];
+// }
+
+// return totalwater;
+        
+//     }
+// }
     }
 }
