dab0b90c0d10d921dbff21891c7373f27f308f85
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-05-15 21:58:49 +0530

    Day X: question name

--- array2.java
+++ array2.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class array2 {
     public static void main(String[] args) {
@@ -360,5 +362,57 @@ public class array2 {
         
 //     }
 // }
+
+// 73. Set Matrix Zeroes
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+// You must do it in place.
+// Example 1:
+
+
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+// Example 2:
+
+
+// Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[0].length
+// 1 <= m, n <= 200
+// -231 <= matrix[i][j] <= 231 - 1
+
+// import java.util.*;
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+
+//         Set<Integer> rows = new HashSet<>();
+//     Set<Integer> cols = new HashSet<>();
+
+//     for(int i=0;i<matrix.length;i++){
+//         for(int j=0;j<matrix[0].length;j++){
+//             if(matrix[i][j] ==0){
+//                 rows.add(i);
+//                 cols.add(j);
+//             }
+//         }
+//     }
+
+//     for(int i=0;i<matrix.length;i++){
+//         for(int j=0;j<matrix[0].length;j++){
+//             if(rows.contains(i) || cols.contains(j)){
+//                 matrix[i][j] = 0;
+//             }
+//         }
+//     }
+
+//     }
+// }
+
+
     }
 }
