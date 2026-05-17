51134ffa721c7034ddb05914142b219cfde327fe
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-05-17 23:44:57 +0530

    Day X: question name

--- array2.java
+++ array2.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -464,6 +465,71 @@ public class array2 {
 //                 right--;
 //             }
 //         }
+//     }
+// }
+
+
+// 15. 3Sum
+
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Explanation: 
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
+// Example 2:
+
+// Input: nums = [0,1,1]
+// Output: []
+// Explanation: The only possible triplet does not sum up to 0.
+// Example 3:
+
+// Input: nums = [0,0,0]
+// Output: [[0,0,0]]
+// Explanation: The only possible triplet sums up to 0.
+ 
+
+// Constraints:
+
+// 3 <= nums.length <= 3000
+// -105 <= nums[i] <= 105
+
+
+// import java.util.*;
+
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//        Set<List<Integer>> uniqueTriplets = new HashSet<>();
+
+//        int n=nums.length;
+
+//        for(int i=0 ;i<n;i++){
+//         int target = -nums[i];
+//         Set<Integer> set = new HashSet<>();
+
+//         for(int j=i+1;j<n;j++){
+//             int third = target - nums[j];
+
+//             if(set.contains(third)){
+//                 List<Integer> trip = Arrays.asList(nums[i],nums[j],third);
+
+//                 Collections.sort(trip);
+//                 uniqueTriplets.add(trip);
+//             }
+//             set.add(nums[j]);
+//         }
+//        }
+//        return new ArrayList<>(uniqueTriplets);
 //     }
 // }
     }
