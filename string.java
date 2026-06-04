197c11be95b9d44d44fa70536a67b2f21197ef11
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-06-04 22:41:59 +0530

    Day X: question name

--- string.java
+++ string.java
@@ -114,5 +114,44 @@ class Solution {
         return (int)high;
     }
 }
+
+
+leetcode question 5 
+class Solution {
+
+    int max = 0;
+    int start = 0;
+    int end = 0;
+
+    public boolean isPalindrome(String s, int i, int j){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public String longestPalindrome(String s) {
+        int n = s.length();
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+
+                if(isPalindrome(s, i, j)){
+                    if(j - i + 1 > max){
+                        max = j - i + 1;
+                        start = i;
+                        end = j;
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+}
     }
 }
