46fb1028b84a3e1a3a46624903ad73bfd6b95404
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-15 23:17:24 +0530

    Day X: question name

--- binarysearch.java
+++ binarysearch.java
@@ -44,18 +44,18 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        // while (left <= right) {
+        //     int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return -1;
+        //     if (nums[mid] == target) {
+        //         return mid;
+        //     } else if (nums[mid] < target) {
+        //         left = mid + 1;
+        //     } else {
+        //         right = mid - 1;
+        //     }
+        // }
+        // return -1;
 
 
 
