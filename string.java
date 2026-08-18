d489353aefbe424560a650ac1ad19fffb73c6408
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-19 00:13:25 +0530

    Done changes

--- string.java
+++ string.java
@@ -59,34 +59,34 @@ public class string {
 
 // second and correct approach 
 
- if(s==null) return 0;
+//  if(s==null) return 0;
 
-        s=s.trim();
+//         s=s.trim();
         
-        if(s.length()==0) return 0;
+//         if(s.length()==0) return 0;
         
-        int sign = +1;
-        long ans = 0;
-        if(s.charAt(0) == '-') sign = -1;
+//         int sign = +1;
+//         long ans = 0;
+//         if(s.charAt(0) == '-') sign = -1;
         
-        int MAX = Integer.MAX_VALUE, MIN = Integer.MIN_VALUE;
+//         int MAX = Integer.MAX_VALUE, MIN = Integer.MIN_VALUE;
 		
-		// initiate the starting pointer
-        int i = (s.charAt(0) == '+' || s.charAt(0) == '-') ? 1 : 0;
+// 		// initiate the starting pointer
+//         int i = (s.charAt(0) == '+' || s.charAt(0) == '-') ? 1 : 0;
 		
-        while(i < s.length()) {
-            if(s.charAt(i) == ' ' || !Character.isDigit(s.charAt(i))) break;
-            ans = ans * 10 + s.charAt(i)-'0';
-            // check the conditions
-            if(sign == -1 && -1*ans < MIN) return MIN;
-            if(sign == 1 && ans > MAX) return MAX;
+//         while(i < s.length()) {
+//             if(s.charAt(i) == ' ' || !Character.isDigit(s.charAt(i))) break;
+//             ans = ans * 10 + s.charAt(i)-'0';
+//             // check the conditions
+//             if(sign == -1 && -1*ans < MIN) return MIN;
+//             if(sign == 1 && ans > MAX) return MAX;
             
-            i++;
-        }
+//             i++;
+//         }
         
-        return (int)(sign*ans);
-    }
-}
+//         return (int)(sign*ans);
+//     }
+// }
 
 
 leetcode question 69
