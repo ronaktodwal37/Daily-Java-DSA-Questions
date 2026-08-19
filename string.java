a18d6cb0767b27bbedb01e4e72164a65ef48ffef
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-19 17:47:14 +0530

    Done changes

--- string.java
+++ string.java
@@ -192,11 +192,11 @@ class Solution {
     public boolean isPalindrome(int x) {
         int duplicate = x;
         int reverse = 0;
-        while(x>0){
-            int rem = x%10;
-            reverse = reverse*10 + rem;
-            x = x/10;
-        }
+        // while(x>0){
+        //     int rem = x%10;
+        //     reverse = reverse*10 + rem;
+        //     x = x/10;
+        // }
 
         if(reverse == duplicate){
             return true;
