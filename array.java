1b9683cc62a96e198d4fb624651a3dda0c735f23
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-21 09:24:21 +0530

    Done changes

--- array.java
+++ array.java
@@ -27,22 +27,22 @@
 // -109 <= target <= 109
 // Only one valid answer exists.
 
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         int[] arr = new int [2];
-//         for(int i=0;i<nums.length;i++){
-//             for (int j=0;j<nums.length;j++){
-//                 if(i!=j){
-//                     if(nums[i]+nums[j] == target){
-//                         arr[0]=i;
-//                         arr[1]=j;
-//                     }
-//                 }
-//             }
-//         }
-//         return arr;
-//     }
-// }
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr = new int [2];
+        for(int i=0;i<nums.length;i++){
+            for (int j=0;j<nums.length;j++){
+                if(i!=j){
+                    if(nums[i]+nums[j] == target){
+                        arr[0]=i;
+                        arr[1]=j;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+}
 
 
 // 75. Sort Colors
