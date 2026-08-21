616b07a03223137a9a5b78b6e11f9b2757a4e8dc
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-21 09:25:11 +0530

    Done changes

--- array.java
+++ array.java
@@ -178,19 +178,19 @@ for(int i=0;i<nums.length;i++){
 
 
 
-// class Solution {
-//     public int maxProfit(int[] prices) {
-//        int maxprofit = 0 ;
-//        int min = Integer.MAX_VALUE;
-//        for(int i=0;i<prices.length;i++){
-//         if(prices[i]<min){
-//             min = prices[i];
-//         }
-//         else{
-//             int profit = prices[i] - min;
-//              maxprofit = Math.max(maxprofit, profit);
-//         }
-//        }
+class Solution {
+    public int maxProfit(int[] prices) {
+       int maxprofit = 0 ;
+       int min = Integer.MAX_VALUE;
+       for(int i=0;i<prices.length;i++){
+        if(prices[i]<min){
+            min = prices[i];
+        }
+        else{
+            int profit = prices[i] - min;
+             maxprofit = Math.max(maxprofit, profit);
+        }
+       }
 
 
     //    2nd approach (but time limit exceed)
