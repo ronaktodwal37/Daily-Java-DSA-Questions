3441ec96fb2e216913051af4b456f0cb991168a2
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-21 09:41:00 +0530

    Done changes

--- array.java
+++ array.java
@@ -318,18 +318,18 @@ class Solution {
       }
       }
      }
-int currentsum = 0;
-for(int i =0;i<nums.length;i++){
-currentsum = currentsum + nums[i];
+// int currentsum = 0;
+// for(int i =0;i<nums.length;i++){
+// currentsum = currentsum + nums[i];
 
-if(currentsum>maxsum){
-    maxsum = currentsum;
-}
-if(currentsum<0){
-    currentsum = 0;
-}
-}
-     return maxsum;
+// if(currentsum>maxsum){
+//     maxsum = currentsum;
+// }
+// if(currentsum<0){
+//     currentsum = 0;
+// }
+// }
+//      return maxsum;
 
 
 
