6a77c555ada1a1126d418c33e02775c4f92c69c3
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-21 09:44:13 +0530

    Done changes

--- array.java
+++ array.java
@@ -27,9 +27,9 @@
 // -109 <= target <= 109
 // Only one valid answer exists.
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] arr = new int [2];
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int[] arr = new int [2];
         // for(int i=0;i<nums.length;i++){
         //     for (int j=0;j<nums.length;j++){
         //         if(i!=j){
