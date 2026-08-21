d0b36303bfce13cccef02736b4100633f7d16e4d
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-21 09:41:18 +0530

    Done changes

--- array.java
+++ array.java
@@ -259,17 +259,17 @@ class Solution {
             }
         }
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-nums[count] =nums[i];
-count++;
-            }
-        }
-        for(int i=count;i<nums.length;i++){
-            nums[i] = 0;
-        }
-    }
-}
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]!=0){
+// nums[count] =nums[i];
+// count++;
+//             }
+//         }
+//         for(int i=count;i<nums.length;i++){
+//             nums[i] = 0;
+//         }
+//     }
+// }
 
 
 
