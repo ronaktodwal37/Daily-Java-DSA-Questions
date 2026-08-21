ce9690c48ca2fa2a0a1c48a93a6fbf41feee4a89
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-21 09:43:16 +0530

    Done changes

--- array.java
+++ array.java
@@ -126,17 +126,17 @@ class Solution {
     public int findDuplicate(int[] nums) {
 
 
-int count =0;
-int ans=0;
-HashMap<Integer,Integer> map = new HashMap<>();
-for(int i=0;i<nums.length;i++){
-    if(map.containsKey(nums[i])){
-         map.put(nums[i],map.get(nums[i])+1); 
-    }
-    else{
-          map.put(nums[i],1);
-    }
-}
+// int count =0;
+// int ans=0;
+// HashMap<Integer,Integer> map = new HashMap<>();
+// for(int i=0;i<nums.length;i++){
+//     if(map.containsKey(nums[i])){
+//          map.put(nums[i],map.get(nums[i])+1); 
+//     }
+//     else{
+//           map.put(nums[i],1);
+//     }
+// }
 
 //  for(Map.Entry<Integer,Integer> e: map.entrySet()) {
 //     if(e.getValue() > 1){
