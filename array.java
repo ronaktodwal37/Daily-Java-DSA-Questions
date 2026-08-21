c2230d59b2881fe60165688fe8080b6cad5af180
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-21 09:39:11 +0530

    Done changes

--- array.java
+++ array.java
@@ -489,21 +489,21 @@ class Solution {
         int[] suffix = new int[nums.length];
 
         // prefix 
-        prefix[0] = 1;
-        for(int i=1;i<n;i++){
-            prefix[i] = prefix[i-1]*nums[i-1];
-        }
+        // prefix[0] = 1;
+        // for(int i=1;i<n;i++){
+        //     prefix[i] = prefix[i-1]*nums[i-1];
+        // }
 
-        suffix[n-1] = 1;
-        for(int i=n-2;i>=0;i--){
-            suffix[i] = suffix[i+1]*nums[i+1];
-        }
+        // suffix[n-1] = 1;
+        // for(int i=n-2;i>=0;i--){
+        //     suffix[i] = suffix[i+1]*nums[i+1];
+        // }
 
-        for(int i=0;i<n;i++){
-            answer[i] = prefix[i]*suffix[i];
-        }
+        // for(int i=0;i<n;i++){
+        //     answer[i] = prefix[i]*suffix[i];
+        // }
 
-        return answer;
+        // return answer;
 
         // int n = nums.length;
         // int prefix[] = new int [nums.length];
