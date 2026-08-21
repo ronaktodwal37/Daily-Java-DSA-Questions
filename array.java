7532f7d52c8a36cc5c270a41ebd6bc816066fd4b
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-21 09:25:21 +0530

    Done changes

--- array.java
+++ array.java
@@ -194,17 +194,17 @@ class Solution {
 
 
     //    2nd approach (but time limit exceed)
-    // for(int i=0;i<prices.length;i++){
-    //     for(int j=i+1;j<prices.length;j++){
-    //         int currentprofit = prices[j]-prices[i];
-    //         if(currentprofit>maxprofit){
-    //             maxprofit = currentprofit ;
-    //         }
-    //     }
-    // }
-//        return maxprofit;
-//     }
-// }
+    for(int i=0;i<prices.length;i++){
+        for(int j=i+1;j<prices.length;j++){
+            int currentprofit = prices[j]-prices[i];
+            if(currentprofit>maxprofit){
+                maxprofit = currentprofit ;
+            }
+        }
+    }
+       return maxprofit;
+    }
+}
 
 
 // 283. Move Zeroes
