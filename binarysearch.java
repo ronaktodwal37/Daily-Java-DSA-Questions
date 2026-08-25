916d044c0aae120dcaf0bfa272a0eafaf9a9d9a4
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-25 23:03:38 +0530

    Done changes

--- binarysearch.java
+++ binarysearch.java
@@ -39,13 +39,13 @@ public class binarysearch {
 
 // leetcode question 704
 
-// class Solution {
-//     public int search(int[] nums, int target) {
-//         int left = 0;
-//         int right = nums.length - 1;
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
 
-        // while (left <= right) {
-        //     int mid = left + (right - left) / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
         //     if (nums[mid] == target) {
         //         return mid;
