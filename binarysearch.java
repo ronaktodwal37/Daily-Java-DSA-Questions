cd76884efa05f789dfedf232be68f30be2409493
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-25 23:04:41 +0530

    Done changes

--- binarysearch.java
+++ binarysearch.java
@@ -52,10 +52,10 @@ class Solution {
             } else if (nums[mid] < target) {
                 left = mid + 1;
             } else {
-                right = mid - 1;
-            }
-        }
-        return -1;
+        //         right = mid - 1;
+        //     }
+        // }
+        // return -1;
 
 
 
