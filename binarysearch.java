adb4eb7d6d1e8d9ca2733635de4b31aed3b05641
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-25 23:04:57 +0530

    Done changes

--- binarysearch.java
+++ binarysearch.java
@@ -50,8 +50,8 @@ class Solution {
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
+        //         left = mid + 1;
+        //     } else {
         //         right = mid - 1;
         //     }
         // }
