d32aaaa317a7f972f9c4e0c9ddc4659a69632f78
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-25 23:05:26 +0530

    Done changes

--- binarysearch.java
+++ binarysearch.java
@@ -44,8 +44,8 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        // while (left <= right) {
+        //     int mid = left + (right - left) / 2;
 
         //     if (nums[mid] == target) {
         //         return mid;
