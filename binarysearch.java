f273c6e09a4a7b50901f174d9993f5a3bfbf238e
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-25 23:03:46 +0530

    Done changes

--- binarysearch.java
+++ binarysearch.java
@@ -47,15 +47,15 @@ class Solution {
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-        //     if (nums[mid] == target) {
-        //         return mid;
-        //     } else if (nums[mid] < target) {
-        //         left = mid + 1;
-        //     } else {
-        //         right = mid - 1;
-        //     }
-        // }
-        // return -1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
 
 
 
