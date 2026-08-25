43c734ce1e810c81306812af97e955dd96f1acf6
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-25 23:02:42 +0530

    Done changes

--- binarysearch.java
+++ binarysearch.java
@@ -5,7 +5,7 @@ public class binarysearch {
     public int search(int[] nums, int target) {
         int ans = -1;
         int low = 0;
-//         int high = nums.length-1;
+        int high = nums.length-1;
 
 //         while(low<=high){
 //             int mid = low + (high-low)/2 ;
