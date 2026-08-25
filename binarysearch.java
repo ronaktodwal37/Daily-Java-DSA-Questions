dc436830739e7de71223814d5f1e9e82303e6a71
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-25 23:05:43 +0530

    Done changes

--- binarysearch.java
+++ binarysearch.java
@@ -39,8 +39,8 @@ public class binarysearch {
 
 // leetcode question 704
 
-class Solution {
-    public int search(int[] nums, int target) {
+// class Solution {
+//     public int search(int[] nums, int target) {
         // int left = 0;
         // int right = nums.length - 1;
 
